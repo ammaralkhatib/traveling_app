f674b72703d72524c13c8f47e623333f675d535e
Ammar <ammar.mngr@gmail.com>
2021-04-15 20:18:48 +0200

    app_data

--- lib/app_data.dart
+++ lib/app_data.dart
@@ -1,1 +1,41 @@
-const Categories_data = const [];
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const Categories_data = const [
+  Category(
+    id: 'c1',
+    title: 'جبال',
+    imageUrl:
+        'https://images.unsplash.com/photo-1575728252059-db43d03fc2de?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTh8fG1vdW5hdGluc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=',
+  ),
+  Category(
+    id: 'c2',
+    title: 'بحيرات',
+    imageUrl:
+        'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  Category(
+    id: 'c3',
+    title: 'شواطىء',
+    imageUrl:
+        'https://images.unsplash.com/photo-1493558103817-58b2924bce98?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAxfHxiZWFjaHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  Category(
+    id: 'c4',
+    title: 'صحارى',
+    imageUrl:
+        'https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVzZXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  Category(
+    id: 'c5',
+    title: 'مدن تاريخية',
+    imageUrl:
+        'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fHRyYXZlbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+  Category(
+    id: 'c6',
+    title: 'أخرى',
+    imageUrl:
+        'https://images.unsplash.com/photo-1605540436563-5bca919ae766?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2tpaW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  ),
+];
